6bd4076234a115e98053b653a5101e52025a3075
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-10-24 14:32:25 +0300

    Calculator was done!

---
+++ Lesson_1/task2/task2/task2.py
@@ -0,0 +1,14 @@
+numbA = float(input("Enter first numb >> "))
+numbB = float(input("Enter srcond numb >> "))
+
+action = input("Select action (+, -, *, /) > ")
+
+print("\nResult ")
+if(action == '+'):
+    print(numbA + numbB)
+if(action == '-'):
+    print(numbA - numbB)
+if(action == '*'):
+    print(numbA * numbB)
+if(action == '/'):
+    print(numbA / numbB)
