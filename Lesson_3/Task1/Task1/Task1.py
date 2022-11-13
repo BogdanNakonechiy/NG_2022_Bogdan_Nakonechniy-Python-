96b50e44235584278989ec2242ade0c067d98e85
Bogdan Nakonechniy <nopebogdan@gmail.com>
2022-11-13 20:56:03 +0200

    Calculator on functions

---
+++ Lesson_3/Task1/Task1/Task1.py
@@ -0,0 +1,27 @@
+def sum(firstNumb, secondNumb):
+    print("Sum of numbs - " + str(firstNumb + secondNumb))
+
+def sum(firstNumb, secondNumb):
+    print("Sub of numbs - " + str(firstNumb - secondNumb))
+
+def mult(firstNumb, secondNumb):
+    print("Mult of numbs - " + str(firstNumb * secondNumb))
+
+def div(firstNumb, secondNumb):
+    print("Division of numbs - " + str(firstNumb / secondNumb))
+
+firstNumb = int(input("Enter first numb > "))
+secondNumb = int(input("Enter second numb > "))
+action = input("Action? (+, -, *, /) > ")
+
+if(action == '+'):
+    sum(firstNumb, secondNumb)
+
+if(action == '-'):
+    sub(firstNumb, secondNumb)
+
+if(action == '*'):
+    mult(firstNumb, secondNumb)
+
+if(action == '/'):
+    div(firstNumb, secondNumb)
